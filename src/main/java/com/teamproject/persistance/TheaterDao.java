1d95247b864a51f390d958b552ccf36bc96cc417
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-03 04:44:36 -0500

    Created TheaterDao

--- src/main/java/com/teamproject/persistance/TheaterDao.java
+++ src/main/java/com/teamproject/persistance/TheaterDao.java
@@ -1,4 +1,32 @@
 package com.teamproject.persistance;
 
-public class TheaterDao {
+import com.teamproject.entity.Theater;
+
+import java.util.List;
+
+public class TheaterDao implements Dao<Theater> {
+    @Override
+    public List<Theater> getAll() {
+        return null;
+    }
+
+    @Override
+    public Theater getById(int id) {
+        return null;
+    }
+
+    @Override
+    public int insert(Theater entity) {
+        return 0;
+    }
+
+    @Override
+    public boolean update(Theater entity) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Theater entity) {
+        return false;
+    }
 }
