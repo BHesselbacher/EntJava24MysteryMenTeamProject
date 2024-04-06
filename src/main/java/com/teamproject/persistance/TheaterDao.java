7bb3fa22460f41d9639507bd5507abd48c3e5de1
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-06 08:27:50 -0500

    TheaterDao work

--- src/main/java/com/teamproject/persistance/TheaterDao.java
+++ src/main/java/com/teamproject/persistance/TheaterDao.java
@@ -1,10 +1,15 @@
 package com.teamproject.persistance;
 
+import com.teamproject.entity.SessionFactoryProvider;
 import com.teamproject.entity.Theater;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class TheaterDao implements Dao<Theater> {
+
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
     @Override
     public List<Theater> getAll() {
         return null;
@@ -12,7 +17,10 @@ public class TheaterDao implements Dao<Theater> {
 
     @Override
     public Theater getById(int id) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Theater theater = session.get( Theater.class, id );
+        session.close();
+        return theater;
     }
 
     @Override
---
+++ src/test/java/com/teamproject/persistance/TheaterDaoTest.java
@@ -0,0 +1,10 @@
+package com.teamproject.persistance;
+
+import com.teamproject.entity.Movie;
+import com.teamproject.entity.Theater;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TheaterDaoTest {
+}
