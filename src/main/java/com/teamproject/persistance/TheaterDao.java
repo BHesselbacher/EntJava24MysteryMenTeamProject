d0694bd6a7e7eac350f8a3fcac76e4b1acb71609
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-06 13:25:50 -0500

    Methods to retrieve data

--- src/main/java/com/teamproject/persistance/TheaterDao.java
+++ src/main/java/com/teamproject/persistance/TheaterDao.java
@@ -2,8 +2,12 @@ package com.teamproject.persistance;
 
 import com.teamproject.entity.SessionFactoryProvider;
 import com.teamproject.entity.Theater;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 
 import java.util.List;
 
@@ -12,7 +16,14 @@ public class TheaterDao implements Dao<Theater> {
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
     @Override
     public List<Theater> getAll() {
-        return null;
+        Session session = sessionFactory.openSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Theater> query = builder.createQuery(Theater.class);
+        Root<Theater> root = query.from(Theater.class);
+        List<Theater> theaters = session.createSelectionQuery(query).getResultList();
+
+        session.close();
+        return theaters;
     }
 
     @Override
@@ -25,16 +36,35 @@ public class TheaterDao implements Dao<Theater> {
 
     @Override
     public int insert(Theater entity) {
-        return 0;
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(entity);
+        transaction.commit();
+        id = entity.getTheaterId();
+        session.close();
+
+        return id;
     }
 
     @Override
     public boolean update(Theater entity) {
-        return false;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.merge(entity);
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
     public boolean delete(Theater entity) {
-        return false;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(entity);
+        transaction.commit();
+
+        session.close();
+        return true;
     }
 }
