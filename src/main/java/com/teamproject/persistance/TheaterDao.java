fd5699b59e2296a8bd2dd9b5bdf32da79c2796e4
chstudebaker <56454279+chstudebaker@users.noreply.github.com>
2024-04-09 19:32:15 -0500

    Update TheaterDao.java by implementing generic dao

--- src/main/java/com/teamproject/persistance/TheaterDao.java
+++ src/main/java/com/teamproject/persistance/TheaterDao.java
@@ -2,69 +2,57 @@ package com.teamproject.persistance;
 
 import com.teamproject.entity.SessionFactoryProvider;
 import com.teamproject.entity.Theater;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 
 import java.util.List;
 
 public class TheaterDao implements Dao<Theater> {
 
-    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+    private final SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
     @Override
     public List<Theater> getAll() {
-        Session session = sessionFactory.openSession();
-        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Theater> query = builder.createQuery(Theater.class);
-        Root<Theater> root = query.from(Theater.class);
-        List<Theater> theaters = session.createSelectionQuery(query).getResultList();
-
-        session.close();
-        return theaters;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Theater", Theater.class).list();
+        }
     }
 
     @Override
     public Theater getById(int id) {
-        Session session = sessionFactory.openSession();
-        Theater theater = session.get( Theater.class, id );
-        session.close();
-        return theater;
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Theater.class, id);
+        }
     }
 
     @Override
     public int insert(Theater entity) {
-        int id = 0;
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(entity);
-        transaction.commit();
-        id = entity.getTheaterId();
-        session.close();
-
-        return id;
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+            return entity.getTheaterId();
+        }
     }
 
     @Override
     public boolean update(Theater entity) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.merge(entity);
-        transaction.commit();
-        session.close();
-        return true;
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+            return true;
+        }
     }
 
     @Override
     public boolean delete(Theater entity) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(entity);
-        transaction.commit();
-
-        session.close();
-        return true;
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(entity);
+            transaction.commit();
+            return true;
+        }
     }
 }
